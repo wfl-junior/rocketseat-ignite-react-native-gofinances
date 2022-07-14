56de13bf89e6ab61c5cb4c2e2d9d5bb8117acd3f
Wallace Júnior <wflj1997@hotmail.com>
2022-07-14 13:24:31 -0300

    Primeiro Teste

--- src/__tests__/screens/Profile.spec.tsx
+++ src/__tests__/screens/Profile.spec.tsx
@@ -2,7 +2,8 @@ import { render } from "@testing-library/react-native";
 import { Profile } from "../../screens/Profile";
 
 it("should render the correct name input placeholder", () => {
-  const { debug } = render(<Profile />);
+  const { getByPlaceholderText } = render(<Profile />);
+  const nameInput = getByPlaceholderText("Nome");
 
-  debug();
+  expect(nameInput).toBeTruthy();
 });
