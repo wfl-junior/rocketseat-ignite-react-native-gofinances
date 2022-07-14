0f9c357c9ca909cf9d240bda6c019dbf3c031b81
Wallace Júnior <wflj1997@hotmail.com>
2022-07-14 13:47:50 -0300

    Seletores

--- src/__tests__/screens/Profile.spec.tsx
+++ src/__tests__/screens/Profile.spec.tsx
@@ -1,9 +1,31 @@
 import { render } from "@testing-library/react-native";
 import { Profile } from "../../screens/Profile";
 
-it("should render the correct name input placeholder", () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const nameInput = getByPlaceholderText("Nome");
+describe("Profile screen", () => {
+  it("should render the correct name input placeholder", () => {
+    const { getByTestId } = render(<Profile />);
+    const nameInput = getByTestId("name-input");
 
-  expect(nameInput).toBeTruthy();
+    expect(nameInput).toBeTruthy();
+    expect(nameInput.props.placeholder).toBe("Nome");
+  });
+
+  it("should load user data", () => {
+    const { getByTestId } = render(<Profile />);
+    const nameInput = getByTestId("name-input");
+    const surnameInput = getByTestId("surname-input");
+
+    expect(nameInput).toBeTruthy();
+    expect(nameInput.props.value).toBe("Wallace");
+    expect(surnameInput).toBeTruthy();
+    expect(surnameInput.props.value).toBe("Júnior");
+  });
+
+  it("should render the title correctly", () => {
+    const { getByTestId } = render(<Profile />);
+    const title = getByTestId("title");
+
+    expect(title).toBeTruthy();
+    expect(title.props.children).toBe("Perfil");
+  });
 });
--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -2,9 +2,22 @@ import { Button, Text, TextInput, View } from "react-native";
 
 export const Profile: React.FC = () => (
   <View>
-    <Text>Perfil</Text>
-    <TextInput placeholder="Nome" autoCorrect={false} />
-    <TextInput placeholder="Sobrenome" autoCorrect={false} />
-    <Button title="Salvar" onPress={() => {}} />
+    <Text testID="title">Perfil</Text>
+
+    <TextInput
+      testID="name-input"
+      placeholder="Nome"
+      autoCorrect={false}
+      value="Wallace"
+    />
+
+    <TextInput
+      testID="surname-input"
+      placeholder="Sobrenome"
+      autoCorrect={false}
+      value="Júnior"
+    />
+
+    <Button testID="save-button" title="Salvar" onPress={() => {}} />
   </View>
 );
