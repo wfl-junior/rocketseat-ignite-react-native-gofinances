e3199bb63b8a74b4d189d2713b4ce6241978c58f
Wallace Júnior <wflj1997@hotmail.com>
2022-07-14 15:51:59 -0300

    Utilizando Mocks

--- src/__tests__/hooks/useAuthContext.spec.tsx
+++ src/__tests__/hooks/useAuthContext.spec.tsx
@@ -4,8 +4,26 @@ import {
   useAuthContext,
 } from "../../contexts/AuthContext";
 
+jest.mock("expo-auth-session", () => ({
+  startAsync: async () => ({
+    type: "success",
+    params: {
+      access_token: "fake-access-token",
+    },
+  }),
+}));
+
 describe("useAuthContext hook", () => {
   it("should be able to sign in with Google", async () => {
+    global.fetch = jest.fn(async () => ({
+      json: async () => ({
+        id: "userInfo.id",
+        email: "userInfo.email",
+        name: "userInfo.name",
+        picture: "userInfo.picture",
+      }),
+    })) as jest.Mock;
+
     const { result } = renderHook(useAuthContext, {
       wrapper: AuthContextProvider,
     });
