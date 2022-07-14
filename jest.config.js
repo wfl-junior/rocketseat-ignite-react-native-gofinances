2c422c78b2679a432c10725e3cc6e5fd1a10a92f
Wallace Júnior <wflj1997@hotmail.com>
2022-07-14 14:51:28 -0300

    Testes com Contexto

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   preset: "jest-expo",
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   testRegex: ["(/src/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$"],
   testPathIgnorePatterns: ["/node_modules", "/android", "/ios"],
+  setupFilesAfterEnv: [
+    "@testing-library/jest-native/extend-expect",
+    "jest-styled-components",
+  ],
 };
--- src/__tests__/components/Input.spec.tsx
+++ src/__tests__/components/Input.spec.tsx
@@ -1,6 +1,11 @@
 import { render } from "@testing-library/react-native";
+import { ThemeProvider } from "styled-components/native";
 import { Input } from "../../components/Form/Input";
 import { theme } from "../../global/styles/theme";
+
+const Providers: React.FC = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
 
 describe("Input component", () => {
   it("should have border when active", () => {
@@ -13,11 +18,34 @@ describe("Input component", () => {
         autoCapitalize="none"
         active
       />,
+      { wrapper: Providers },
     );
 
-    const emailInput = getByTestId("email-input");
+    const input = getByTestId("email-input");
+    const inputStyle = input.props.style[0];
 
-    expect(emailInput.props.style[0].borderColor).toBe(theme.colors.attention);
-    expect(emailInput.props.style[0].borderWidth).toBe(3);
+    expect(inputStyle.borderColor).toBe(theme.colors.attention);
+    expect(inputStyle.borderWidth).toBe(3);
+    expect(inputStyle.borderStyle).toBe("solid");
+  });
+
+  it("should not have border when not active", () => {
+    const { getByTestId } = render(
+      <Input
+        testID="email-input"
+        placeholder="E-mail"
+        keyboardType="email-address"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />,
+      { wrapper: Providers },
+    );
+
+    const input = getByTestId("email-input");
+    const inputStyle = input.props.style[0];
+
+    expect(inputStyle.borderColor).toBeUndefined();
+    expect(inputStyle.borderWidth).toBeUndefined();
+    expect(inputStyle.borderStyle).toBeUndefined();
   });
 });
